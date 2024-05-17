1b75cc16fc469b5ae204f6f0e4651465b8d5f5d5
uhnv <58312711+uhnv@users.noreply.github.com>
2024-05-16 20:31:09 -0700

    Issue: not all entries were on map, SOLVED.

--- jsonToMap.js
+++ jsonToMap.js
@@ -1,5 +1,5 @@
 // Initialize the map
-var map = L.map('map').setView([20, 10], 3); // Centered at (20,10) with zoom level 3
+var map = L.map('map').setView([20, 10], 2); // Centered at (20,10) with zoom level 3
 
 // Add the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,22 +8,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Fetch the JSON data from the NASA API URL
 fetch('https://data.nasa.gov/api/views/gh4g-9sfh/rows.json?accessType=DOWNLOAD')
-.then(response => response.json())
-.then(data => {
-// Extract the rows containing meteorite data
-var rows = data.data;
+  .then(response => response.json())
+  .then(data => {
+    // Extract the rows containing meteorite data
+    var rows = data.data;
 
-/* 
- * Parse the rows of data from JSON, retrieve needed information, 
- * and add this information to the map using markers and pop-up windows
-*/
-rows.forEach(function(row) {
-    let latitude = parseFloat(row[15]);
-    let longitude = parseFloat(row[16]);
-    L.marker([latitude, longitude]).addTo(map)
-    .bindPopup(`Place: ${row[8]}, <br>Mass (g): ${row[12]}, <br>Fell status: ${row[13]}, <br> Year: ${row[14]}`)
-    //.openPopup();
+    /* 
+     * Parse the rows of data from JSON, retrieve needed information, filter out invalid data, 
+     * add each geolocation to the map using circles and additional info using pop-up windows.
+    */
+    let meteoriteCounter = 0;
+    let invalidDataCounter = 0;
+    rows.forEach(function (row) {
+      const latitude = parseFloat(row[15]);
+      const longitude = parseFloat(row[16]);
+      if (!isNaN(latitude) && !isNaN(longitude)) {
+        L.circleMarker([latitude, longitude]).addTo(map)
+          .bindPopup(`Place: ${row[8]}, <br>Mass (g): ${row[12]}, <br>Fell status: ${row[13]}, <br> Year: ${row[14]}`);
+        //.openPopup();
+        meteoriteCounter++;
+      } else { 
+        invalidDataCounter++;
+        //console.warn(`Invalid latitude or longitude value in row: ${row}`);
+      }
     });
-    
-})
-.catch(error => console.error('Error fetching data:', error));
+    // Print out the valid/invalid, and total number of meteorites on the map
+    console.log(`Invalid data entries count: ${invalidDataCounter}`)
+    console.log(`Valid data entries count: ${meteoriteCounter}`);
+    console.log(`Total data entries count: ${meteoriteCounter + invalidDataCounter}`);
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
